ce5081974ae405d987d2abc55c704333a45d7c61
luismorel11 <luismorel11@live.com>
2022-05-02 20:37:42 -0400

    Se agrega el Proyecto

---
+++ Aplicacion de Cifrado/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Aplicacion_de_Cifrado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbEncrypt = new System.Windows.Forms.RadioButton();
+            this.rbDecrypt = new System.Windows.Forms.RadioButton();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(223, 259);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(276, 31);
+            this.tbPassword.TabIndex = 0;
+            this.tbPassword.TextChanged += new System.EventHandler(this.tbPassword_TextChanged);
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBrowse.Location = new System.Drawing.Point(603, 112);
+            this.btBrowse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(106, 32);
+            this.btBrowse.TabIndex = 1;
+            this.btBrowse.Text = "Examinar";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Segoe UI", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btStart.Location = new System.Drawing.Point(37, 367);
+            this.btStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(608, 101);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Iniciar";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ruta de Archivo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // rbEncrypt
+            // 
+            this.rbEncrypt.AutoSize = true;
+            this.rbEncrypt.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbEncrypt.Location = new System.Drawing.Point(63, 170);
+            this.rbEncrypt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbEncrypt.Name = "rbEncrypt";
+            this.rbEncrypt.Size = new System.Drawing.Size(139, 36);
+            this.rbEncrypt.TabIndex = 6;
+            this.rbEncrypt.TabStop = true;
+            this.rbEncrypt.Text = "Encriptar";
+            this.rbEncrypt.UseVisualStyleBackColor = true;
+            this.rbEncrypt.CheckedChanged += new System.EventHandler(this.rbEncrypt_CheckedChanged);
+            // 
+            // rbDecrypt
+            // 
+            this.rbDecrypt.AutoSize = true;
+            this.rbDecrypt.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDecrypt.Location = new System.Drawing.Point(262, 170);
+            this.rbDecrypt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbDecrypt.Name = "rbDecrypt";
+            this.rbDecrypt.Size = new System.Drawing.Size(179, 36);
+            this.rbDecrypt.TabIndex = 7;
+            this.rbDecrypt.TabStop = true;
+            this.rbDecrypt.Text = "Desencriptar";
+            this.rbDecrypt.UseVisualStyleBackColor = true;
+            this.rbDecrypt.CheckedChanged += new System.EventHandler(this.rbDecrypt_CheckedChanged);
+            // 
+            // tbPath
+            // 
+            this.tbPath.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbPath.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPath.Location = new System.Drawing.Point(63, 112);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(509, 34);
+            this.tbPath.TabIndex = 8;
+            this.tbPath.TextChanged += new System.EventHandler(this.tbPath_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.rbDecrypt);
+            this.Controls.Add(this.rbEncrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbPassword);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Cifrado/Decifrado de archivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbEncrypt;
+        private System.Windows.Forms.RadioButton rbDecrypt;
+        private System.Windows.Forms.TextBox tbPath;
+    }
+}
+
---
+++ Aplicacion de Cifrado/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Aplicacion_de_Cifrado
+{
+    public partial class Form1 : Form
+    {
+        byte[] abc;
+        byte[,] table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = od.FileName;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            rbEncrypt.Checked = true;
+
+            abc = new byte[256];
+            for (int i = 0; i < 256; i++)
+                abc[i] = Convert.ToByte(i);
+
+            table = new byte[256, 256];
+            for (int i = 0; i < 256; i++)
+                for (int j = 0; j < 256; j++)
+                {
+                    table[i, j] = abc[(i + j) % 256];
+                }
+
+        }
+
+        private void rbEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbEncrypt.Checked)
+            {
+                rbDecrypt.Checked = false;
+            }
+
+        }
+
+        private void rbDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDecrypt.Checked)
+                rbEncrypt.Checked = false;
+
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(tbPath.Text))
+            {
+                MessageBox.Show("El archivo no existe.");
+                return;
+            }
+            if (String.IsNullOrEmpty(tbPassword.Text))
+            {
+                MessageBox.Show("Ingrese la contraseña");
+                return;
+            }
+            //key = 10 
+
+
+            try
+            {
+                byte[] fileContent = File.ReadAllBytes(tbPath.Text);
+                byte[] passwordTmp = Encoding.ASCII.GetBytes(tbPassword.Text);
+                byte[] keys = new byte[fileContent.Length];
+                for (int i = 0; i < fileContent.Length; i++)
+                    keys[i] = passwordTmp[i % passwordTmp.Length];
+
+
+                byte[] result = new byte[fileContent.Length];
+
+                if (rbEncrypt.Checked)
+                {
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        byte value = fileContent[i];
+                        byte key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                            if (abc[j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        result[i] = table[keyIndex, valueIndex];
+                    }
+                }
+
+                else
+                {
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        byte value = fileContent[i];
+                        byte key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                            if (abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (table[keyIndex, j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        result[i] = abc[valueIndex];
+                    }
+                }
+
+
+                String fileExt = Path.GetExtension(tbPath.Text);
+                SaveFileDialog sd = new SaveFileDialog();
+                sd.Filter = "Files (*" + fileExt + ") | *" + fileExt;
+                if (sd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(sd.FileName, result);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("se esta usando el archivo, intentalo nuevamente");
+                return;
+
+            }
+        }
+
+        private void tbPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
